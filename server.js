16426cf14193cad3c8d62b6bd60ff035b39527ab
Ubuntu <ubuntu@ip-172-31-27-19.us-west-2.compute.internal>
2022-11-16 06:19:59 +0000

    Redirect http to https.

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const fs = require('fs')
-const https = require('https')
+const https = require('https');
 
 const app = express();
 
+// Redirect http to https.
+app.use((request, response, next) => {
+    if (request.secure) {
+	next();
+    } else {
+	return response.redirect("https://" + request.headers.host + request.url);
+    }
+});
+
+// Serve static files.
 app.use(express.static(__dirname + '/static', { dotfiles: 'allow' }));
 
 app.get('/', (req, res) => {
     res.send('Hello HTTPS!')
-})
+});
 
-const port = 443;
+// Start the https webserver.
 const httpsOptions = {
     key: fs.readFileSync('/etc/letsencrypt/live/rustgovernment.com/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/rustgovernment.com/fullchain.pem'),
 };
-const server = https.createServer(httpsOptions, app);
-server.listen(port, () => {
-    console.log(`Webserver listening on port ${port}.`)
-});
+https.createServer(httpsOptions, app).listen(443);
+
+// Run an http webserver whole only job is to redirect http to https.
+app.listen(80);
