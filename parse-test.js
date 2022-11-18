48788a0f112a345520e6af8965b5844dbfc16cae
Ubuntu <ubuntu@ip-172-31-27-19.us-west-2.compute.internal>
2022-11-18 03:38:40 +0000

    HTML parsing works.

--- parse-test.js
+++ parse-test.js
@@ -3,9 +3,11 @@ const fs = require('fs');
 const ParseHtml = require('./parse');
 
 describe('ParseHtml', function() {
-    it('Parses HTML.', () => {
-	const html = fs.readFileSync('example-login-response.html');
+    it('Parses token & steam ID from a realistic response.', () => {
+	const buffer = fs.readFileSync('example-login-response.html');
+	const html = buffer.toString();
 	const p = ParseHtml(html);
-	assert(p === 'abc');
+	assert(p.steamId === '86561198054245954');
+	assert(p.token === 'eyJzdGVhbUlkIjoiNzY1MjExOTgwNTQyNDU5NTUiLCJpc3MiOjE2Njg3MzU3MzUsImV4cCI6MTY2OTk0NTMzNX0=.NaEEKRijVshN3xFJQ1h5NVORvMt9Wg1BkRxJXkG0WHCsccS3lQlhA7bYTsKQASFrqvG2Zc/pQozvs/m3HDVRCQ==');
     });
 });
--- parse.js
+++ parse.js
@@ -1,7 +1,45 @@
-
+// Parses a token & SteamID out of an HTML web page.
+// The page is a response after the user logs in to
+// the Rust+ companion login page.
 
 function ParseHtml(html) {
-    return 'abc';
+    const genericError = { error: `That's not the right text to paste. Please check the instructions above and try again.` };
+    if (html.length < 200) {
+	return genericError;
+    }
+    if (html.length > 5000) {
+	return genericError;
+    }
+    const htmlPosition = html.indexOf(`<html`);
+    if (htmlPosition < 0) {
+	return genericError;
+    }
+    const steamIdMarker = String.raw`{\"SteamId\":\"`;
+    const steamIdPosition = html.indexOf(steamIdMarker);
+    if (steamIdPosition < 0) {
+	return genericError;
+    }
+    const steamIdOnwards = html.substring(steamIdPosition + steamIdMarker.length);
+    const steamIdEnd = steamIdOnwards.indexOf(`\\`);
+    if (steamIdEnd !== 17) {
+	return genericError;
+    }
+    const steamId = steamIdOnwards.substring(0, steamIdEnd);
+    const tokenMarker = String.raw`\"Token\":\"`;
+    const tokenPosition = html.indexOf(tokenMarker);
+    if (tokenPosition < 0) {
+	return genericError;
+    }
+    const tokenOnwards = html.substring(tokenPosition + tokenMarker.length);
+    const tokenEnd = tokenOnwards.indexOf(`\\`);
+    if (tokenEnd < 0) {
+	return genericError;
+    }
+    const token = tokenOnwards.substring(0, tokenEnd);
+    if (token.length < 50 || token.length > 500) {
+	return genericError;
+    }
+    return { steamId, token };
 }
 
 module.exports = ParseHtml;
