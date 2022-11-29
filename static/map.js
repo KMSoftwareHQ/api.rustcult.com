c9f2c577237cfacb5dea35b7c73d45bea063c3b4
Ubuntu <ubuntu@ip-172-31-27-19.us-west-2.compute.internal>
2022-11-29 23:44:59 +0000

    Made main canvas with background color.

--- static/map.js
+++ static/map.js
@@ -1,12 +1,29 @@
 let cachedMapData = null;
 
+const mapCanvas = document.getElementById('mapcanvas');
+const mapContext = mapCanvas.getContext('2d');
+
+function Draw() {
+    const map = cachedMapData.map;
+    mapContext.fillStyle = map.background;
+    mapContext.fillRect(0, 0, mapCanvas.width, mapCanvas.height);
+}
+
+function OnResize() {
+    mapCanvas.width = window.innerWidth;
+    mapCanvas.height = window.innerHeight;
+    Draw();
+}
+
+window.addEventListener('resize', OnResize, false);
+
 async function Main() {
-    const mapImageTag = document.getElementById('mapimage');
     const response = await fetch('/mapdata');
     const mapData = await response.json();
     cachedMapData = mapData;
     console.log(mapData);
-    mapImageTag.src = 'data:image/png;base64, ' + mapData.map.jpgImage;
+    //mapImageTag.src = 'data:image/png;base64, ' + mapData.map.jpgImage;
+    OnResize();
 }
 
 Main();
