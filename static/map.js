baed45e6a9041f37782986e8a9d9c201f4db9934
Ubuntu <ubuntu@ip-172-31-27-19.us-west-2.compute.internal>
2022-12-01 04:30:55 +0000

    Fixed map scaling.

--- static/map.js
+++ static/map.js
@@ -13,25 +13,31 @@ function Sleep(ms) {
 }
 
 function Draw() {
+    if (!cachedMapData) {
+	console.log('No cached map data. Bailing.');
+	return;
+    }
     const map = cachedMapData.map;
     mapContext.fillStyle = map.background;
     mapContext.fillRect(0, 0, mapCanvas.width, mapCanvas.height);
     const w = mapCanvas.width;
     const h = mapCanvas.height;
     const wh = Math.min(w, h);
-    const mw = cachedMapData.map.width;
-    const mh = cachedMapData.map.height;
-    const oceanMargin = cachedMapData.map.oceanMargin;
-    mapContext.drawImage(mapImageTag, oceanMargin, oceanMargin, mw - 2 * oceanMargin, mh - 2 * oceanMargin, (w - wh) / 2, (h - wh) / 2, wh, wh);
+    const mw = map.width;
+    const mh = map.height;
+    const om = map.oceanMargin;
+    mapContext.drawImage(
+	mapImageTag,
+	om, om, mw - 2 * om, mh - 2 * om,
+	(w - wh) / 2, (h - wh) / 2, wh, wh);
     const cx = w / 2;
     const cy = h / 2;
     const ox = cx - wh / 2;
     const oy = cy + wh / 2;
     const mapSize = 4500;
-    for (const monument of cachedMapData.map.monuments) {
+    for (const monument of map.monuments) {
 	const x = ox + wh * monument.x / mapSize;
 	const y = oy - wh * monument.y / mapSize;
-	console.log(x, y);
 	mapContext.fillStyle = '#0000FF';
 	mapContext.beginPath();
 	mapContext.arc(x, y, 3, 0, 2 * Math.PI);
@@ -51,7 +57,6 @@ async function Main() {
     const response = await fetch('/mapdata');
     const mapData = await response.json();
     cachedMapData = mapData;
-    console.log(mapData);
     await Sleep(100);
     mapImageTag = document.createElement('img');
     mapImageTag.src = 'data:image/png;base64, ' + mapData.map.jpgImage;
