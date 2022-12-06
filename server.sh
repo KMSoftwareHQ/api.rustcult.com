24e42559a0497c965779eee8ce06e29255d9668b
Ubuntu <ubuntu@ip-172-31-27-19.us-west-2.compute.internal>
2022-12-06 00:06:30 +0000

    Animate dots.

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 while :
 do
-    echo "Starting the server"
+    echo "Starting the server."
     sudo timeout 3600 /usr/local/n/versions/node/16.18.1/bin/node server.js
     echo "Server stopped. Restarting shortly."
     sleep 1
--- static/map.js
+++ static/map.js
@@ -1,6 +1,9 @@
 let cachedMapData;
 let mapImageTag;
 let cachedDots;
+let cachedDotsTime;
+let previousCachedDots;
+let previousCachedDotsTime;
 
 const mapCanvas = document.getElementById('mapcanvas');
 const mapContext = mapCanvas.getContext('2d');
@@ -39,22 +42,36 @@ function Draw() {
     const oy = cy + wh / 2;
     const mapSize = 4500;
 
-    function DrawDots(dots, borderColor, fillColor) {
+    function DrawDots(dots, oldDots, alpha, borderColor, fillColor) {
 	if (!dots) {
 	    return;
+	}
+	const beforeAndAfter = {};
+	for (const dot of dots) {
+	    beforeAndAfter[dot.steamId] = { after: dot };
+	}
+	if (oldDots) {
+	    for (const dot of oldDots) {
+		beforeAndAfter[dot.steamId].before = dot;
+	    }
 	}
 	mapContext.fillStyle = fillColor;
 	mapContext.strokeStyle = borderColor;
 	mapContext.lineWidth = 2;
-	for (const dot of dots) {
+	const r = alpha;
+	for (const steamId in beforeAndAfter) {
+	    const dot = beforeAndAfter[steamId].after;
+	    const oldDot = beforeAndAfter[steamId].before;
 	    const threshold = 0.001;
 	    if (Math.abs(dot.x) <= threshold && Math.abs(dot.y) <= threshold) {
 		continue;
 	    }
-	    const x = ox + wh * dot.x / mapSize;
-	    const y = oy - wh * dot.y / mapSize;
+	    const x = oldDot ? r * dot.x + (1 - r) * oldDot.x : dot.x;
+	    const y = oldDot ? r * dot.y + (1 - r) * oldDot.y : dot.y;
+	    const px = ox + wh * x / mapSize;
+	    const py = oy - wh * y / mapSize;
 	    mapContext.beginPath();
-	    mapContext.arc(x, y, 3, 0, 2 * Math.PI);
+	    mapContext.arc(px, py, 3, 0, 2 * Math.PI);
 	    mapContext.stroke();
 	    mapContext.fill();
 	}
@@ -62,9 +79,13 @@ function Draw() {
 
     DrawDots(map.monuments, '#db4437', 'rgba(234, 153, 153, 0.5)');
     if (cachedDots) {
-	DrawDots(cachedDots.enemies, '#FFF000', 'rgba(255, 240, 0, 0.8)');
-	DrawDots(cachedDots.allies, '#00FFF0', 'rgba(0, 255, 240, 0.8)');
-	DrawDots(cachedDots.team, '#00FF00', 'rgba(182, 215, 168, 0.8)');
+	const currentTime = new Date().getTime();
+	const timeFraction = (currentTime - 1000 - previousCachedDotsTime) / (cachedDotsTime - previousCachedDotsTime);
+	const alpha = Math.max(0, Math.min(1, timeFraction));
+	const prev = previousCachedDots || {};
+	DrawDots(cachedDots.enemies, prev.enemies, alpha, '#FFF000', 'rgba(255, 240, 0, 0.8)');
+	DrawDots(cachedDots.allies, prev.allies, alpha, '#00FFF0', 'rgba(0, 255, 240, 0.8)');
+	DrawDots(cachedDots.team, prev.team, alpha, '#00FF00', 'rgba(182, 215, 168, 0.8)');
     }
 }
 
@@ -79,13 +100,20 @@ window.addEventListener('resize', OnResize, false);
 async function FetchDots() {
     const response = await fetch('/dots');
     const jsonResponse = await response.json();
+    previousCachedDots = cachedDots;
+    previousCachedDotsTime = cachedDotsTime;
     cachedDots = jsonResponse.dots;
+    cachedDotsTime = new Date().getTime();
 }
 
-async function PeriodicUpdate() {
+async function PeriodicUpdateForDotsData() {
     await FetchDots();
+    setTimeout(PeriodicUpdateForDotsData, 1000);
+}
+
+async function DoFrame() {
     Draw();
-    setTimeout(PeriodicUpdate, 1000);
+    setTimeout(DoFrame, 10);
 }
 
 async function Main() {
@@ -97,7 +125,8 @@ async function Main() {
     mapImageTag.src = 'data:image/png;base64, ' + mapData.map.jpgImage;
     await Sleep(100);
     OnResize();
-    PeriodicUpdate();
+    await PeriodicUpdateForDotsData();
+    await DoFrame();
 }
 
 Main();
