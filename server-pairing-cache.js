96a8c4a46ea9cc08a7996254e05f44bdfde0cb15
Ubuntu <ubuntu@ip-172-31-27-19.us-west-2.compute.internal>
2022-12-07 19:08:48 +0000

    Ripped rustplus out of server pairing cache.

--- server-pairing-cache.js
+++ server-pairing-cache.js
@@ -1,6 +1,5 @@
 const db = require('./database');
 const moment = require('moment');
-const rustplus = require('./rustplus');
 
 class ServerPairing {
     constructor(databaseRow) {
@@ -9,7 +8,6 @@ class ServerPairing {
 	this.token = databaseRow.token;
 	this.consecutiveFailureCount = databaseRow.consecutive_failure_count;
 	this.nextRetryTime = databaseRow.next_retry_time;
-	this.rustPlusClient = null;
     }
 
     async SetToken(token) {
@@ -74,16 +72,6 @@ class ServerPairing {
 	    await this.SetToken(message.playerToken);
 	}
     }
-
-    async ConnectRustPlus() {
-	const tokens = this.serverHostAndPort.split(':');
-	if (tokens.length !== 2) {
-	    throw 'Server pairing with invalid host and port.';
-	}
-	const host = tokens[0];
-	const port = parseInt(tokens[1]);
-	this.rustPlusClient = await rustplus.Connect(host, port, this.userSteamId, this.token);
-    }
 }
 
 let pairingsByHostPortAndSteamId = {};
@@ -94,7 +82,6 @@ async function Initialize() {
     const results = await db.Query('SELECT * from server_pairings');
     for (const row of results) {
 	const pairing = new ServerPairing(row);
-	//await pairing.ConnectRustPlus();
 	const cacheKey = pairing.serverHostAndPort + ':' + pairing.userSteamId;
 	newCache[cacheKey] = pairing;
     }
@@ -123,7 +110,6 @@ async function CreateNewPairingInDatabase(message) {
     }
     const row = results[0];
     const pairing = new ServerPairing(row);
-    await pairing.ConnectRustPlus();
     const cacheKey = pairing.serverHostAndPort + ':' + pairing.userSteamId;
     pairingsByHostPortAndSteamId[cacheKey] = pairing;
     return pairing;
