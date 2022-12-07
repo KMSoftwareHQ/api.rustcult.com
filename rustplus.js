dff58a0db2d0c3a531e5b01d87a056da680ad91d
Ubuntu <ubuntu@ip-172-31-27-19.us-west-2.compute.internal>
2022-12-07 19:10:01 +0000

    All requests to rustplus are now stateless.

--- rustplus.js
+++ rustplus.js
@@ -1,18 +1,5 @@
 const RustPlus = require('rustplus-api');
 
-function Connect(host, port, steamId, token) {
-    return new Promise((resolve, reject) => {
-	const client = new RustPlus(host, port, steamId, token);
-	client.on('connected', () => resolve(client));
-    });
-}
-
-function SendRequest(client, request) {
-    return new Promise((resolve, reject) => {
-	client.sendRequest(request, resolve);
-    });
-}
-
 function OneOffRequest(serverPairingRecord, request) {
     const hostAndPort = serverPairingRecord.serverHostAndPort;
     const tokens = hostAndPort.split(':');
@@ -53,7 +40,5 @@ function OneOffRequest(serverPairingRecord, request) {
 }
 
 module.exports = {
-    Connect,
     OneOffRequest,
-    SendRequest,
 };
